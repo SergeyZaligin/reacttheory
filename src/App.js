2a64d6f08ba36554aa84b6de11e21812b3459c08
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-29 14:17:23 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,89 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Car from './Car/Car';
+import Car from "./Car/Car";
 
 class App extends Component {
+  /*
+  * State есть только у компонетов наследуемых Component(extends Component)
+  */
+  state = {
+    cars: [
+      {
+        id: 1,
+        name: "Ford",
+        year: 2018
+      },
+      {
+        id: 2,
+        name: "Mazda",
+        year: 2017
+      },
+      {
+        id: 3,
+        name: "Gaz",
+        year: 1990
+      }
+    ],
+    pageTitle: "Cars list",
+    showCars: false
+  };
+  changeTitleHandler = newTitle => {
+    this.setState({
+      pageTitle: newTitle
+    });
+  };
+  handleInput = e => {
+    this.setState({
+      pageTitle: e.target.value
+    });
+  };
+  showCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    });
+  };
+  onChangeName = (name, index) => {
+    const car = this.state.cars[index];
+    car.name = name;
+    const cars = [...this.state.cars];
+    cars[index] = car;
+    this.setState({
+      cars
+    });
+  };
+  handlerDeleteCar(index) {
+    const cars = [...this.state.cars];
+    cars.splice(index, 1);
+    this.setState({
+      cars
+    });
+  }
   render() {
+    console.log("render");
     return (
-      <div className="App">   
-          <Car name={ 'Ford' } year={ '2018' } />
-          <hr/>
-          <Car name={ 'Mazda' } year={ '2017' } />
-          <hr/>
-          <Car name={ 'Gaz' } year={ '2000' } />
+      <div className="App">
+        <h1>{this.state.pageTitle}</h1>
+        <input type="text" onChange={this.handleInput} />
+        <button onClick={this.changeTitleHandler.bind(this, "Change")}>
+          Change title
+        </button>
+        <button onClick={this.showCarsHandler}>
+          {this.state.showCars ? "Hide cars" : "Show cars"}
+        </button>
+        {this.state.showCars
+          ? this.state.cars.map((car, index) => {
+              return (
+                <Car
+                  onDelete={this.handlerDeleteCar.bind(this, index)}
+                  key={car.id}
+                  onChangeName={e => this.onChangeName(e.target.value, index)}
+                  name={car.name}
+                  year={car.year}
+                />
+              );
+            })
+          : null}
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 /**
  * Простой компонент без стейта
  */
 
 export default props => (
-    <div>
-        <h3>Car: { props.name }</h3>
-        <strong>Year: { props.year }</strong>  
-    </div>
+  <div
+    style={{
+      border: "1px solid #ccc",
+      marginBottom: "10px",
+      padding: "10px",
+      display: "block"
+    }}
+  >
+    <h3>Car: {props.name}</h3>
+    <strong>Year: {props.year}</strong>
+    <input type="text" onChange={props.onChangeName} value={props.name} />
+    <button onClick={props.onDelete}>Delete</button>
+  </div>
 );
